1ace08d5a37c0dea4e06038a80ef482847ba897d
gatotoadrian <gatotoadrian@gmail.com>
2022-05-04 14:54:20 +0300

    heroku commits

--- NewsAPI-master/run.py
+++ NewsAPI-master/run.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-from newsapi import NewsApiClient
+# from newsapi import NewsApiClient
+# from newsapi import NewsApiClient
+# from newsapi import NewsApiClient
+from newsapi.newsapi_client import NewsApiClient
+
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-
 
     newsapi = NewsApiClient(api_key="1111f8f3bf334cc6a0af77fa30f68e29")
 
